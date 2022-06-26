ef0dcab245a443d02887e9ad9e2150060e79b443
Александр Мацнев <Top_proger@outlook.com>
2022-06-26 21:58:17 +0300

    Модели дня и недели

---
+++ Models/Day.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_ToDoList.Models
+{
+    internal class Day
+    {
+        public string Name { get; set; }
+        public int MonthDate { get; set; }
+        public string Month { get; set; }
+    }
+}
