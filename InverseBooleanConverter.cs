751f7ee42dcc4b8933ac6b0fb6ee9f23098735bf
Александр Мацнев <Top_proger@outlook.com>
2022-06-26 16:58:34 +0300

    Конвертер булевых значений

---
+++ InverseBooleanConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPF_ToDoList
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    internal class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+            {
+                throw new InvalidOperationException("Invalid data type: bool required.");
+            }
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
