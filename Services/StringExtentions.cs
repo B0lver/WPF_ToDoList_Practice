f4dd41303421109c5079ba3483ad59594e95b199
Александр Мацнев <Top_proger@outlook.com>
2022-07-20 17:32:32 +0300

    Добавление расширения для получения строки с первой заглавной буквой.

---
+++ Services/StringExtentions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_ToDoList.Services
+{
+    public static class StringExtentions
+    {
+        public static string FirstLetterToUpper(string input)
+        {
+            return input switch
+            {
+                null => throw new ArgumentNullException(nameof(input)),
+                "" => throw new ArgumentException($"String cannot be empty: {nameof(input)}"),
+                _ => String.Concat(input[0].ToString().ToUpper(), input.AsSpan(2))
+            };
+        }
+    }
+}
