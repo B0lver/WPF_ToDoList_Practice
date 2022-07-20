3a7016e253ad4a3b4b0be4116e6bbed17abe12db
Александр Мацнев <Top_proger@outlook.com>
2022-07-20 21:59:02 +0300

    Изменение модели.

--- Models/ToDoItem.cs
+++ Models/ToDoItem.cs
@@ -1,8 +1,12 @@
-﻿namespace WPF_ToDoList.Models
+﻿using System;
+
+namespace WPF_ToDoList.Models
 {
-    internal class ToDoItem
+    public class ToDoItem
     {
+        public Guid Id { get; set; }
+        public DateOnly Date { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string Text { get; set; }
     }
 }
