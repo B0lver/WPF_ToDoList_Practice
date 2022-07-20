26ac3ef4d7cbcf8a4b6df328eaf1b18eab547598
Александр Мацнев <Top_proger@outlook.com>
2022-07-20 21:58:51 +0300

    Добавление контекста приложения.

---
+++ DbConnection/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_ToDoList.Models;
+
+namespace WPF_ToDoList.DbConnection
+{
+    public class ApplicationContext : DbContext
+    {
+        private const string ConnectionString = @"Data Source = DESKTOP-FRSJ73N\SQLEXPRESS\WPF_ToDoApp";
+
+        public DbSet<ToDoItem> Items => Set<ToDoItem>();
+        public ApplicationContext() => Database.OpenConnection();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(ConnectionString);
+        }
+    }
+}
